19d335d2b41f79b3aeaa6ba7abcbd58f51f2d6b7
MitM123 <mitmonpara23@gmail.com>
2025-06-13 22:01:44 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex bg-amber-400 justify-center'>
+        This is Home Page
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div className='flex bg-amber-400 justify-center h-screen items-center'>
+            This is Genuin Video Editor App
+        </div>
+    )
+}
+
+export default Home
